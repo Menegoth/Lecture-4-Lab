b265de857721e2726fa54ac84fe785f8da3a935c
Antonio <antonioareas@me.com>
2020-09-27 13:21:08 -0700

    original commit

--- Lecture 4/MainWindow.xaml.cs
+++ Lecture 4/MainWindow.xaml.cs
@@ -34,7 +34,67 @@ namespace Lecture_4
         {
 
             //take in label values
+            int currentSpeed = Int32.Parse(speedTextBox.Text);
+            int speedLimit = Int32.Parse(limitTextBox.Text);
+
+            int fine;
+
+            //check if over speed limit
+            if (currentSpeed < speedLimit)
+            {
+                //set background and message
+                fineCanvas.Background = Brushes.Green;
+                resultLabel.Content = "You are driving under the speed limit.";
+
+            } else if (currentSpeed == speedLimit)
+            {
+                //set background and message
+                fineCanvas.Background = Brushes.Yellow;
+                resultLabel.Content = "You are matching the speed limit";
+
+            } else
+            {
+
+                //calculate fine with function and assign it to variable
+                fine = calculateFine(currentSpeed, speedLimit);
+
+                //set background and message
+                fineCanvas.Background = Brushes.Red;
+                resultLabel.Content = "You are going over the speed limit. Your fine is " + fine;
+
+            }
 
         }
+
+        //calculate fine function
+        public int calculateFine(int speed, int limit)
+        {
+
+            //declare 
+            int fine = 0;
+            int startingFine = 60;
+            int incrementFine = 7;
+            int extremeFine = 250;
+
+            //calculate fine
+            if (speed < limit + 25)
+            {
+                //calculate without 25mph+ fine
+                fine = startingFine + (incrementFine * (speed - limit));
+
+            } else
+            {
+                //calculate with 25mph+ fine
+                //calculate original fine first
+                fine = startingFine + (incrementFine * (speed - limit));
+
+                //add 25mph+ fine
+                fine += (extremeFine * ((speed - limit) / 25));
+
+            }
+
+            return fine;
+        }
+
     }
 }
